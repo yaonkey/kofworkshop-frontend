aa87ab448052ea9f5117edfc49548d99d3eec20b
yaonkey <d.dovganiuk@.>
2024-08-28 13:19:04 +0300

    staged

---
+++ server/api/set_session.js
@@ -0,0 +1,14 @@
+// import kv from '@vercel/kv';
+// export default defineEventHandler(async () => {
+	// await kv.set('session', {admin: true});
+	// const session = await kv.get('session'); // {admin: true}
+// 	return {
+// 		session: session
+// 	};
+// });
+
+
+// import { sql } from "@vercel/postgres";
+
+// const likes = 100;
+// const { rows } = await sql`SELECT * FROM posts WHERE likes > ${likes};`;
