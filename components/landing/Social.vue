6789b456883cf694a594fd2475f2ad58acb874ea
yaonkey <thesuperuserstyle@gmail.com>
2024-09-06 22:59:58 +0300

    change animation

--- components/landing/Social.vue
+++ components/landing/Social.vue
@@ -6,7 +6,7 @@
         <NuxtImg src="/icons/vk.svg" alt="VK icon" loading="eager" format="avif" width="50" height="50" />
       </LandingLink>
       <LandingLink size="lg" styleName="outline" rel="noopener" href="https://t.me/kofworkshop" target="_blank"
-        class="rounded-xl animate-[bounce_1s_ease-in-out_infinite]">
+        class="rounded-xl animate-[bounce_1.5s_ease-in-out_infinite]">
         <NuxtImg src="/icons/telegram.svg" alt="Telegram icon" loading="eager" format="avif" width="50" height="50" />
       </LandingLink>
       <LandingLink size="lg" styleName="outline" rel="noopener" href="https://reddit.com/r/kofworkshop" target="_blank"
@@ -16,7 +16,7 @@
     </div>
     <div class="flex flex-row gap-2 items-center justify-center">
       <LandingLink size="lg" styleName="outline" rel="noopener" href="https://pikabu.ru/@megawhat" target="_blank"
-        class="rounded-xl animate-[bounce_1s_ease-in-out_infinite]">
+        class="rounded-xl animate-[bounce_1.5s_ease-in-out_infinite]">
         <NuxtImg src="/icons/pikabu.svg" alt="Pikabu icon" loading="eager" format="avif" width="50" height="50" />
       </LandingLink>
       <LandingLink size="lg" styleName="outline" rel="noopener" href="https://www.twitch.tv/bakasempaides"
@@ -24,7 +24,7 @@
         <NuxtImg src="/icons/twitch.svg" alt="Twitch icon" loading="eager" format="avif" width="50" height="50" />
       </LandingLink>
       <LandingLink size="lg" styleName="outline" rel="noopener" href="https://instagram.com/kofworkshop/"
-        target="_blank" class="rounded-xl animate-[bounce_1s_ease-in-out_infinite]">
+        target="_blank" class="rounded-xl animate-[bounce_1.5s_ease-in-out_infinite]">
         <NuxtImg src="/icons/instagram.svg" alt="Instagram icon" loading="eager" format="avif" width="50" height="50" />
       </LandingLink>
     </div>
