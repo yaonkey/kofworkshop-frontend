68525365da7513b2f3d1f7344bdc1b14c7eee4ae
yaonkey <thesuperuserstyle@gmail.com>
2024-10-14 02:39:30 +0300

    change admin username and password

--- server/api/auth/login.ts
+++ server/api/auth/login.ts
@@ -4,8 +4,11 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { username, password } = body;
 
+  const validUsername = process.env.USERNAME;
+  const validPassword = process.env.PASSWORD;
+
   // Здесь должна быть ваша логика проверки логина и пароля
-  if (username === 'admin' && password === 'password') {
+  if (username === validUsername && password === validPassword) {
     // Успешная авторизация
     // Например, можно установить cookie или сессию
     return { success: true };
